c6aaff5bf7e190abe6d3fd30658ca77a67fdcfb5
65536william <75908561+65536william@users.noreply.github.com>
2022-12-01 22:58:33 +0000

    add first paper

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -9,24 +9,70 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
+      author: {
+        Row: {
+          id: string
+          created_at: string
+          updated_at: string
+          name: string
+        }
+        Insert: {
+          id?: string
+          created_at?: string
+          updated_at?: string
+          name: string
+        }
+        Update: {
+          id?: string
+          created_at?: string
+          updated_at?: string
+          name?: string
+        }
+      }
       paper: {
         Row: {
           id: string
           created_at: string
           updated_at: string
           title: string
+          abstract: string | null
         }
         Insert: {
           id?: string
           created_at?: string
           updated_at?: string
           title: string
+          abstract?: string | null
         }
         Update: {
           id?: string
           created_at?: string
           updated_at?: string
           title?: string
+          abstract?: string | null
+        }
+      }
+      paper_author: {
+        Row: {
+          created_at: string
+          updated_at: string
+          paper_id: string
+          author_id: string
+          position: number | null
+        }
+        Insert: {
+          created_at?: string
+          updated_at?: string
+          paper_id: string
+          author_id: string
+          position?: number | null
+        }
+        Update: {
+          created_at?: string
+          updated_at?: string
+          paper_id?: string
+          author_id?: string
+          position?: number | null
         }
       }
     }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,43 @@ import { Database } from "../lib/database.types";
 
 export default function Index() {
   const supabaseClient = useSupabaseClient<Database>();
-  const [data, setData] = useState<any>();
+  const [papers, setPapers] =
+    useState<Awaited<ReturnType<typeof fetchPapers>>>();
+
+  async function fetchPapers() {
+    const { data } = await supabaseClient
+      .from("paper")
+      .select("*, paper_author ( author (*), position )");
+    return data;
+  }
 
   useEffect(() => {
-    async function loadData() {
-      const { data } = await supabaseClient.from("paper").select("*");
-      setData(data);
+    async function fetchData() {
+      const data = await fetchPapers();
+      setPapers(data);
     }
-    loadData();
+    fetchData();
   }, []);
 
   return (
     <div>
       <h1>Paperwatch</h1>
-      <h2>Test</h2>
-      {data?.map((paper: any) => (
+      {papers?.map((paper) => (
         <div key={paper.id}>
           <span>{paper.title}</span>
+          <p>{paper.abstract}</p>
+          <div>
+            {Array.isArray(paper.paper_author) &&
+              paper.paper_author
+                .sort((a, b) => (a.position || 0) - (b.position || 0))
+                .map((paper_author) => (
+                  // @ts-ignore
+                  <div key={paper.id + "-" + paper_author.author?.id}>
+                    {/* @ts-ignore */}
+                    {paper_author.author?.name}
+                  </div>
+                ))}
+          </div>
         </div>
       ))}
     </div>
